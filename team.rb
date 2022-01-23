a5bcc24baa52ed1a7dc123067e6b3aaaceacc452
Ubuntu <ubuntu@ip-172-31-15-53.ap-northeast-1.compute.internal>
2022-01-23 22:28:35 +0000

    ver.2

--- team.rb
+++ team.rb
@@ -1,6 +1,6 @@
 class Team
     
-    attr_accessor :name,:win,:lose,:draw    
+    attr_accessor :name,:win,:lose,:draw 
     
     def initialize(name,win,lose,draw)
         self.name = name
@@ -9,27 +9,28 @@ class Team
         self.draw = draw
     end
     
+        def calc_win_rate
+        win.to_f/(win+lose)
+       
+        end
     
-    def calc_win_rate(a)
-        a=win.to_f/win.to_f+lose.to_f
-    end
-    
-    def show_team_result(a)
-        "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は #{calc_win_rate}"
+    def show_team_result()
+       puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、#勝率は {calc_win_rate}"
     end
 end
-    giants = team.new(giants,67,45,8)
-    tigers = team.new(tigers,60,53,7)
-    dragons = team.new(dragons,60,55,5)
-    baystars = team.new(baystars,56,58,6)
-    carp = team.new(carp,52,56,12)
-    swallows = team.new(swallows,41,69,10)
-    
-    giants.show_team_result(a)
-    tigers.show_team_result(a)
-    dragons.show_team_result(a)
-    baystars.show_team_result(a)
-    carp.show_team_result(a)
-    swallows.show_team_result(a)
+    giants = Team.new("giants",67,45,8)
+    tigers = Team.new("tigers",60,53,7)
+    dragons = Team.new("dragons",60,55,5)
+    baystars = Team.new("baystars",56,58,6)
+    carp = Team.new("carp",52,56,12)
+    swallows = Team.new("swallows",41,69,10)
 
-    
+    #giants.calc_win_rate
+    #a="aaaaa"
+    giants.show_team_result
+    tigers.show_team_result
+    dragons.show_team_result
+    baystars.show_team_result
+    carp.show_team_result
+    swallows.show_team_result
+
